ef2286b423913e6c908577edb0d86c22edc89a52
david shibley <david.shibley@contentful.com>
2025-02-28 14:56:48 -0700

    Convert function-potterdb-rest-api from react scripts to vite

---
+++ v2/examples/function-potterdb-rest-api/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useMemo } from 'react';
+import { locations } from '@contentful/app-sdk';
+import Field from './locations/Field';
+import { useSDK } from '@contentful/react-apps-toolkit';
+import Dialog from './locations/Dialog';
+
+const ComponentLocationSettings = {
+  [locations.LOCATION_ENTRY_FIELD]: Field,
+  [locations.LOCATION_DIALOG]: Dialog,
+};
+
+const App = () => {
+  const sdk = useSDK();
+
+  const Component = useMemo(() => {
+    for (const [location, component] of Object.entries(ComponentLocationSettings)) {
+      if (sdk.location.is(location)) {
+        return component;
+      }
+    }
+  }, [sdk.location]);
+
+  return Component ? <Component /> : null;
+};
+
+export default App;
---
+++ v2/examples/function-potterdb-rest-api/src/hooks/useCharacters.ts
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+import { CharacterAttributes, CharacterProps } from '../types';
+
+async function fetchCharacters(filter: string): Promise<CharacterAttributes[]> {
+  const characterProps: CharacterProps[] = await fetch(
+    `https://api.potterdb.com/v1/characters?filter[name_cont]=${filter}`
+  )
+    .then((response) => response.json())
+    .then((data) => data.data);
+
+  return characterProps
+    .map((character) => ({ ...character.attributes }))
+    .filter((character) => character.image !== null);
+}
+
+type HookResult = {
+  characters?: CharacterAttributes[];
+  isLoading: boolean;
+};
+
+export function useCharacters(filter: string): HookResult {
+  const { isLoading, data: characters } = useQuery<CharacterAttributes[]>({
+    queryKey: ['characters', filter],
+    queryFn: () => fetchCharacters(filter),
+    retry: false,
+  });
+
+  return { characters, isLoading };
+}
